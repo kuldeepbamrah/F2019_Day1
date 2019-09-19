8a5730259f2a0873b08b14574478041a7a3ec18a
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-09-19 06:05:33 +0530

    Initial commit

---
+++ src/com/lambton/FirstDay.java
@@ -0,0 +1,13 @@
+package com.lambton;
+
+public class FirstDay
+{
+    public static void main(String[] args)
+    {
+       int a,b,c;
+       a=10;
+       b=10;
+       c=a+b;
+       System.out.println("SUM: " + c);
+    }
+}
